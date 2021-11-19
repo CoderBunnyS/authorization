fd1157bcd127f0e73a6ab8932260e1629fde8cd3
Bunny Schaefer <SchaeferBunny@gmail.com>
2021-11-18 21:23:58 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express()
+
+app.get("/public", (req, res) => {
+    res.status(200).send("Hello Public");
+});
+
+app.get("/private", (req, res) => {
+    res.status(200).send("Hello Private")
+});
+app.get("*", (req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(8888, () => console.log("API started on port 8888"))
